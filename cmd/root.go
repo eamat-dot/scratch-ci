e5fce2a06dfc7fe576790bebcf36960cc4c11fce
eamat. <eamat.dot@gmail.com>
2026-06-24 22:40:12 +0900

    refactor(cmd): ♻️ コマンドの初期化処理を整理
    Change:
      - 設定ファイルのパスを XDG Base Directory Specification に従って修正
      - コメントを日本語に翻訳し、可読性を向上

--- cmd/root.go
+++ cmd/root.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -15,7 +16,7 @@ const APP_NAME string = "__REPLACE_MODULE_NAME__"
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
+// rootCmd は、サブコマンドなしで呼び出された場合の基本コマンドを表します。
 var rootCmd = &cobra.Command{
 	Use:   APP_NAME,
 	Short: "アプリケーションの簡単な説明",
@@ -24,8 +25,8 @@ var rootCmd = &cobra.Command{
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// Execute は、すべての子コマンドをルート コマンドに追加し、フラグを適切に設定します。
+// これは main.main() によって呼び出されます。これは rootCmd に対して 1 回だけ実行する必要があります。
 func Execute() {
 	rootCmd.Version = VersionString()
 	err := rootCmd.Execute()
@@ -41,37 +42,25 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/."+APP_NAME+".yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is ~/.config/"+APP_NAME+"/config.toml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-	// rootフラグのサンプル
-	// rootCmd.PersistentFlags().BoolVarP(
-	// 	&debug,
-	// 	"debug",
-	// 	"d",
-	// 	false,
-	// 	"デバッグモード",
-	// )
-
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
+	if cfgFile != "" { // --config 指定時
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
+		configDir, err := getConfigDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".__REPLACE_MODULE_NAME__" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName("." + APP_NAME)
+		viper.AddConfigPath(filepath.Join(configDir, APP_NAME))
+		viper.SetConfigType("toml")
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
@@ -80,4 +69,16 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+// getConfigDir は、XDG Base Directory Specification に従って設定ディレクトリを取得する
+func getConfigDir() (string, error) {
+	if dir := os.Getenv("XDG_CONFIG_HOME"); dir != "" {
+		return dir, nil
+	}
+
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	return filepath.Join(home, ".config"), nil
 }
